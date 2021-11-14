35569438af2c148a1ba8745d187cb5e0035b6696
hurui <58035322+hurui@users.noreply.github.com>
2021-11-14 20:03:13 +0800

    Add 404 not found page

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::{
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:10000").unwrap();
 
+    println!("listening on http://127.0.0.1:10000");
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -21,10 +23,16 @@ fn handle_connection(mut stream: TcpStream) {
 
     if buffer.starts_with(get) {
         let contents = fs::read_to_string("hello.html").unwrap();
-
         let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
         stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let reponse = format!("{}{}", status_line, contents);
+
+        stream.write(reponse.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
 }
